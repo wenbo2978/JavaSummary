07a3b826cde8114109723ee8739dc69f6e2123e4
wenbo <wenbochen@1997.gmail.com>
2024-10-14 16:12:32 -0400

    add new file

---
+++ src/com/mockInterview/stream/StreamSolution.java
@@ -0,0 +1,22 @@
+package com.mockInterview.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamSolution {
+
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 6, 7, 8, 9, 10);
+
+        List<Integer> processedNumbers = numbers.stream()
+                .filter(num -> num > 5)
+                .map(num -> num * 2)
+                .collect(Collectors.toList());
+        System.out.println(processedNumbers);
+
+        int sum = numbers.stream()
+                .reduce(0, (subTotal, ele) -> subTotal + ele);
+        System.out.println(sum);
+    }
+}
