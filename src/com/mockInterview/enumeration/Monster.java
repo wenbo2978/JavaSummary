741ddfc4d2bf281242953c5414e80e018d02fa3d
wenbo <wenbochen@1997.gmail.com>
2024-11-29 18:14:06 -0500

    update file

---
+++ src/com/mockInterview/enumeration/Monster.java
@@ -0,0 +1,24 @@
+package com.mockInterview.enumeration;
+
+public enum Monster {
+    EASY("H", 20, 30),
+    NORMAL("K", 25, 40),
+    HARD("L", 30, 50),
+    MASTER("O", 80, 200),
+    BOSS("Dragon", 150, 120000);
+    public String name;
+    public int attack;
+    public int life;
+
+    Monster(String name, int attack, int life){
+        this.name = name;
+        this.attack = attack;
+        this.life = life;
+    }
+
+    public String printInfo(){
+        return this + " : " + "name: " + this.name + ", attack: " + this.attack + ", life: " + this.life;
+    }
+
+
+}
---
+++ src/com/mockInterview/enumeration/Solution.java
@@ -0,0 +1,8 @@
+package com.mockInterview.enumeration;
+
+public class Solution {
+    public static void main(String[] args){
+        Monster monster = Monster.BOSS;
+        System.out.println(monster.printInfo());
+    }
+}
